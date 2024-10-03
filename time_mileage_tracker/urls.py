3d8f6d35cfab7e697ed062f1b6482aec6207b7dc
Ryan <rtdilbert2@gmail.com>
2024-10-03 14:21:18 -0400

    adds basic login and forgot password requests/ as well as mitigates the need for /users/

--- time_mileage_tracker/urls.py
+++ time_mileage_tracker/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
 from django.shortcuts import render
+from django.urls import path
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
+from time_mileage_tracker.views import home_view
+
 
 # Simple function-based view for the homepage
 def index_view(request):
@@ -8,6 +12,17 @@ def index_view(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('users.urls')),
+    # path('regisration/', include('users- DNU.urls')),
     path('', index_view, name='index'),  # Root URL points to the index view
+
+    path('home/', home_view, name='home'),  # Root URL points to the index view
+
+     path('login/', LoginView.as_view(), name='login'),
+    path('', home_view, name='home'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 ]
---
+++ time_mileage_tracker/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
+
+
+def login_view(request):
+    if request.method == 'POST':
+        # Handle POST request: Process the login form data
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to a home or dashboard page
+            else:
+                return HttpResponse("Invalid login")
+        else:
+            return HttpResponse("Invalid form data")
+    else:
+        # Handle GET request: Display the login form
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+def home_view(request):
+    return render(request, 'home.html')  # Replace with your actual template
