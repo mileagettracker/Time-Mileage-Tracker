1ba5dacf9a7d6056e2114cb005f2627b35788826
parthpatel <parth2274@gmail.com>
2024-10-23 19:17:37 -0400

    Cleaned up dashboard and did some style changes. 10-23-24

--- time_mileage_tracker/settings.py
+++ time_mileage_tracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-93x@)w(wnc22s)i74n)44ttt)kri7cr6m76tfoosu)wv%^ttx#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '10.0.0.206', '127.0.0.1']
 
 
 # Application definition
@@ -122,7 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Additional locations the staticfiles app will traverse
-STATICFILES_DIRS = [BASE_DIR / "static"]  # This includes your project-level 'static/' directory
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # This includes your project-level 'static/' directory
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
